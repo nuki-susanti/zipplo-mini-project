d0e2f6fde53c6b7522e4926093a82d214314c509
dsitnic <dennis-sitnic@hotmail.de>
2026-06-02 08:22:59 +0200

    added database names

--- sql/datawarehouse/customer_ETL.sql
+++ sql/datawarehouse/customer_ETL.sql
@@ -1,21 +1,18 @@
-DELETE FROM dim_customer;
-
-INSERT INTO dim_customer ( 
-	customer_name, 
-	[type], 
-	company, 
-	customer_alt_key
-) 
+INSERT INTO ZipploDW.dbo.dim_customer (
+    customer_name,
+    [type],
+    company,
+    customer_alt_key
+)
 SELECT 
-	c.name AS customer_name, 
-	c.type AS [type], 
-	c.company AS company, 
-	c.customer_id AS customer_alt_key 
-FROM Customer AS c;
-
+    c.name AS customer_name,
+    c.type AS [type],
+    c.company AS company,
+    c.customer_id AS customer_alt_key
+FROM ZipploDB.dbo.Customer AS c
 -- alt
 WHERE NOT EXISTS (
-	SELECT 1 
-	FROM dim_customer AS dc 
-	WHERE dc.customer_alt_key = c.customer_id 
+    SELECT 1
+    FROM ZipploDW.dbo.dim_customer AS dc
+    WHERE dc.customer_alt_key = c.customer_id
 );
